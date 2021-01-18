92e56a2c3722d1f44f11dc904a4f209540f7fa28
Sanat Raorane <sanatraorane@outlook.com>
2021-01-19 01:24:45 +0530

    Android Manifest updated

--- app/src/main/java/com/example/android/hawk/MainActivity.java
+++ app/src/main/java/com/example/android/hawk/MainActivity.java
@@ -5,21 +5,77 @@ package com.example.android.hawk;
  * MainActivity class, launcher activity
  */
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox adminEnabled;
+    private DevicePolicyManager devicePolicyManager;
+ //   private ActivityManager activityManager;
+    private ComponentName compName;
+    private boolean isAdminActive;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isAdminActive = devicePolicyManager.isAdminActive(compName);
+        //First Set the CheckBox
+        if(!isAdminActive && adminEnabled.isChecked()){
+                adminEnabled.toggle();
+        }
+        else if(isAdminActive && !adminEnabled.isChecked()){
+            adminEnabled.toggle();
+        }
+        //Prompt User To Enable Admin privilege
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.O) //Requires minimum Oreo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adminEnabled=findViewById(R.id.adminAccessCheckBox);
+        devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+     //   activityManager=(ActivityManager)getSystemService(ACTIVITY_SERVICE);
+        compName = new ComponentName(this,loginWatch.class);
+
         if(!SecurityService.serviceRunning){
             startForegroundService(new Intent(MainActivity.this,SecurityService.class));
         }
+
+        adminEnabled.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Admin Access Needed To Check If Incorrect PIN Entered");
+                startActivityForResult(intent,3 );  //request code 3
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 3:
+                if (resultCode == RESULT_OK)
+                    Toast.makeText(MainActivity.this, "Admin Privilege Granted", Toast.LENGTH_SHORT);
+                else
+                    Toast.makeText(MainActivity.this, "Failed to Enable Admin Privilege", Toast.LENGTH_SHORT).show();
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
--- app/src/main/java/com/example/android/hawk/loginWatch.java
+++ app/src/main/java/com/example/android/hawk/loginWatch.java
@@ -10,17 +10,20 @@ import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 public class loginWatch extends DeviceAdminReceiver {
     @Override
     public void onEnabled(@NonNull Context context, @NonNull Intent intent) {
+        Toast.makeText(context,"Device Admin Enabled",Toast.LENGTH_SHORT).show();
         Log.i("loginWatch","Device Admin Enabled");
     }
 
     @Override
     public void onDisabled(@NonNull Context context, @NonNull Intent intent) {
+        Toast.makeText(context,"Device Admin Disabled",Toast.LENGTH_SHORT).show();
         Log.i("loginWatch","Device Admin Disabled");
     }
 
@@ -29,6 +32,6 @@ public class loginWatch extends DeviceAdminReceiver {
     public void onPasswordFailed(@NonNull Context context, @NonNull Intent intent) {
         super.onPasswordFailed(context, intent);
         SecurityService.failedPasswordCount++;
-        Log.i("loginWatch-passFailed", String.valueOf(SecurityService.failedPasswordCount));
+        Log.d("loginWatch-passFailed", String.valueOf(SecurityService.failedPasswordCount));
     }
 }
